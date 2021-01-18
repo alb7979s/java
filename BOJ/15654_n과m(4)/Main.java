43392583a25629379747147959406749c56cd0f5
jonghyeok <alb7979s@naver.com>
2021-01-18 17:15:51 +0900

    순열

--- BOJ/15654_n과m(4)/Main.java
+++ BOJ/15654_n과m(4)/Main.java
@@ -1,6 +1,8 @@
-// n°úm(1)
+// n°úm(5)
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -9,27 +11,33 @@ public class Main {
 	static boolean[] visit;
 	static Stack<Integer> stack = new Stack<>();
 	static int[] arr;
+	static StringBuilder sb; 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
+		sb = new StringBuilder();
 		arr = new int[n];
 		visit = new boolean[n];
+		st = new StringTokenizer(br.readLine(), " ");
 		for(int i=0; i<n; i++) arr[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
 		solve(0);
+		System.out.println(sb);
 	}
 	
-	public static void solve(int cnt) {
+	public static void solve(int cnt) throws IOException {
 		if(cnt == m ) {
 			for(int x : stack) {
-				System.out.print(x + " ");
+				sb.append(x);
+				sb.append(" ");
 			}
-			System.out.println();
+			sb.append("\n");
 			return ;
 		}
 		for(int i=0; i<n; i++) {
-			if (!visit[i]) {
+			if(!visit[i]) {
 				visit[i] = true;
 				stack.push(arr[i]);
 				solve(cnt+1);
--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,6 +1,5 @@
-//n°úm(5)
-public class Main {
 
+public class Main {
 	public static void main(String[] args) {
 		
 	}
