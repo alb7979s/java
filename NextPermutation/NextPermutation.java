e8c9b8ddd8b9534ecfcbeaa82b4f2b4285900709
jonghyeok <alb7979s@naver.com>
2021-02-16 10:09:49 +0900

    nextPermutation 설명 추가

--- NextPermutation/NextPermutation.java
+++ NextPermutation/NextPermutation.java
@@ -26,19 +26,24 @@ public class NextPermutation {
 		input[j] = temp;
 	}
 	
+	// 아래 예제 그려보면 이해하기 쉬움
+	// 12543 -> 13245
+	// 13245 -> 13254
 	public static boolean nextPermutation() {
-		// 꼭대기 찾음
+		// 1. 뒤에서 보면서 꺾이는 지점 찾음(가장 큰 값이 아니라 뒤랑 비교했을 때 커지는 값)
 		int i = n - 1;
 		// 앞쪽에 있는 녀석이 같거나 클 때 => 전진(i>0인 동안)
 		while( i > 0 && input[i-1] >= input[i] ) i--;
 		// 더이상 앞자리가 없는 상황: 현 순열의 상태가 가장 큰순열(마지막 순열)
 		if(i==0) return false;
-		// 꼭대기 찾음
+		
+		// 2. 뒤쪽부터 탐색하여 교환할 위치 찾기(i-1보다 큰 값중 가장 오른쪽 값)
 		int j = n - 1;
 		while( input[i-1] >= input[j] ) j--;
-		
+		// 3. 두 위치 값 교환
 		swap(i-1, j);
 		
+		// 4. 꼭대기부터 맨 뒤까지 오름차순 정렬(
 		int k = n - 1;
 		while( i < k ) {
 			swap(i++, k--);
